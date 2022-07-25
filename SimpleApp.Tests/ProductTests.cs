316457d0ffb6091acb1af3af66c68657665fbfa5
Roman Dzygun <dzygun-roman@mail.ru>
2022-07-25 14:14:22 +0300

    Testing

---
+++ SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimpleApp.Models;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            //Организация
+            var p = new Product { Name = "Test", Price = 100M };
+            //Действие
+            p.Name = "New Name";
+            //Утверждение
+            Assert.Equal("New name", p.Name);
+        }
+
+    }
+}
