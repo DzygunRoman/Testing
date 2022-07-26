03922c32417f3d9ed2f94a125bbd051a1f552ad4
Roman Dzygun <dzygun-roman@mail.ru>
2022-07-26 09:59:46 +0300

    Testing

--- SimpleApp.Tests/ProductTests.cs
+++ SimpleApp.Tests/ProductTests.cs
@@ -16,8 +16,17 @@ namespace SimpleApp.Tests
             //Действие
             p.Name = "New Name";
             //Утверждение
-            Assert.Equal("New name", p.Name);
+            Assert.Equal("New Name", p.Name);
         }
-
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            //Организация
+            var p = new Product { Name = "Test", Price = 100M };
+            //Действие
+            p.Price = 200M;
+            //Утверждение
+            Assert.Equal(200M, p.Price);
+        }
     }
 }
